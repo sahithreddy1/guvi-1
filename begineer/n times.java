4b5d86823321d714e9d94408e37ea371312f1219
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-07-31 10:34:17 +0530

    Create n times.java

---
+++ begineer/n times.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string");
+		int n=sc.nextInt();
+		System.out.println("repeat number of times");
+		for(int i=0;i<=n;i++)
+		System.out.println("hello");
+		
+	}
+}
