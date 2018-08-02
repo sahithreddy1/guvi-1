ccad38b0a5ec78993ac1f7506b6b8d0bcb18e91f
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-02 16:00:23 +0530

    Create armstrong.java

---
+++ begineer/armstrong.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number, temp,total=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		number=n;
+		while(n!=0)
+		{
+			temp=n%10;
+			total=total+temp*temp*temp;
+			n=n/10;
+		}
+		if(number==total)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
