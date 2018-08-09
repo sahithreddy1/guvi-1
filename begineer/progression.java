353d8ab92f6504ba1f0986b1d754dcd911a33906
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-09 11:00:00 +0530

    Create progression.java

---
+++ begineer/progression.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int d=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+a;
+			a=a+d;
+		}
+		System.out.println(sum);
+		
+		
+			
