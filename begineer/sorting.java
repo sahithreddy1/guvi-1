1c3ce99afa617ec426076505cd65482fa602937d
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-10 14:02:54 +0530

    Create sorting.java

---
+++ begineer/sorting.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		int array[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			array[i]=sc.nextInt();
+			
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(array[i]>array[j])
+				{
+				int temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+				
+				}
+		
+			}
+		}	
+		for(int i=0;i<n-1;i++)
+		{
+			System.out.print(array[i]+" ");
+		}
+		System.out.print(array[n-1]);
+		
+	}
+}
