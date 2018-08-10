cef73b09d0d1efe63e7c21d5e3a22c9d9961f418
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-10 11:53:01 +0530

    Create maxarray.java

---
+++ begineer/maxarray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		int array[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		int larg=array[0];
+		int i=0;
+		while(i<n)
+		{
+			if(larg>array[i])
+			{
+				larg=array[i];
+			}
+			i++;
+		}
+		System.out.print(larg);
+	}
+	
+	
+}
