ec71d01b97bcbe16f929c83859437fcb7b059cf8
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-14 10:07:08 +0530

    Create countnumeric.java

---
+++ begineer/countnumeric.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int numbers=0;
+			Scanner sc=new Scanner(System.in);
+			String n=sc.nextLine();
+			for(int i=0;i<n.length();i++)
+			{
+				char ch=n.charAt(i);
+				if((ch>='A')&&(ch<='z'))
+				{
+				}	
+				else if((ch>='a')&&(ch<='z'))
+				{
+				}
+				else if((ch>='0')&&(ch<='9'))
+				{
+					numbers++;
+				}
+				else 
+				{
+				}
+			}
+			System.out.println(numbers+" ");
+	}
+}
