07b942a55a5d6066bf3363ce72b5591762e3c7ec
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-08-16 10:37:58 +0530

    Create program45.java

---
+++ begineer/program45.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		while(n!=0)
+		{
+			n=n/10;
+			sum++;
+		}
+		System.out.print(sum);
+	}
+}
